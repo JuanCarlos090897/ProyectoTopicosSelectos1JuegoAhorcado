1d621143802ed976feafb02b9a4d9ffe55fb727b
JuanCarlos090897 <36517516+JuanCarlos090897@users.noreply.github.com>
2019-07-10 19:55:49 -0600

    Probando WCF
    Esto es una prueba nada mas

---
+++ ProyectoAhorcadoWCF/ClienteWS/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ClienteWS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdentificacion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(262, 27);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Identificacion";
+            // 
+            // txtIdentificacion
+            // 
+            this.txtIdentificacion.Location = new System.Drawing.Point(127, 30);
+            this.txtIdentificacion.Name = "txtIdentificacion";
+            this.txtIdentificacion.Size = new System.Drawing.Size(100, 20);
+            this.txtIdentificacion.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 165);
+            this.Controls.Add(this.txtIdentificacion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBuscar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdentificacion;
+    }
+}
+
---
+++ ProyectoAhorcadoWCF/ProyectoAhorcadoWCF/IWSPersonas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ProyectoAhorcadoWCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+
+    //[ServiceContract] // Esto lo que hace es que cada persona que se mete a mi Web Service puede utilizar los siquientes metodos 
+
+    //[DataContract]// esto lo que hace es avisarle al WS que va a manejar el WS 
+
+    //[DataMember]// esto se lo debemos de poner a los metodos que queremos exponer 
+
+
+
+    [ServiceContract]
+    public interface IWSPersonas
+    {
+        [OperationContract]
+        Persona ObtenerPersona(String Identificacion);
+    }
+    [DataContract]
+    public class Persona : BaseRespuesta //lo que hago aqui es heredar de una clase de respuestas que la defino abajo
+    {
+        [DataMember]
+        public string Nombre { get; set; }
+        [DataMember]
+        public int Edad { get; set; }
+    }
+    [DataContract]
+    public class BaseRespuesta
+    {
+        [DataMember]
+        public string MensajeRespueta { get; set; }
+        [DataMember]
+        public string Error { get; set; }
+    }
+}
